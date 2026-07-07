bf0272000847b4f21a7e1ba64d426349cf132698
Szym03 <123407078+Szym03@users.noreply.github.com>
2026-07-07 18:18:42 +0200

    further home structuring

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav>
+      {/* TODO: logo, e.g. <span>noisy</span> */}
+      {/* TODO: two buttons on the right: help ("?") and menu (burger) */}
+    </nav>
+  );
+}
+
+export default Navbar;
